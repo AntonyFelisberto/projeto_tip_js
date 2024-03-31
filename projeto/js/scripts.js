f3948ce92261b8ffb6c2ef682e644ff72682f66e
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-03-31 17:24:46 -0300

    finalizando

--- projeto/js/scripts.js
+++ projeto/js/scripts.js
@@ -1,7 +1,13 @@
 function formatMoney(value){
+    value = Math.ceil(value * 100) / 100;
     value = value.toFixed(2);
     return `$ ${value}`
 }  
+
+function formatSplit(value){
+    if(value === 1) return value + " person"
+    return value + " people"
+}
 
 function update() {
     let bill = Number(document.getElementById("yourBill").value)
@@ -15,6 +21,6 @@ function update() {
     document.getElementById("tipPercent").innerHTML = tipPercent + " %"
     document.getElementById("tipValue").innerHTML = formatMoney(tipValue)
     document.getElementById("totalWithTip").innerHTML = formatMoney(billTotal)
-    document.getElementById("splitValue").innerHTML = split
+    document.getElementById("splitValue").innerHTML = formatSplit(split)
     document.getElementById("billEach").innerHTML =  formatMoney(billEach)
 }
