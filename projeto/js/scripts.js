0d22f9c7ebb165c4d714ac26faadf3b1d5c9168c
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-03-31 17:06:19 -0300

    iniciando js

---
+++ projeto/js/scripts.js
@@ -0,0 +1,5 @@
+function update() {
+    let bill = Number(document.getElementById("yourBill").value)
+    let tipPercent = document.getElementById("tipPercent").value
+    let split = document.getElementById("splitInput").value
+}
