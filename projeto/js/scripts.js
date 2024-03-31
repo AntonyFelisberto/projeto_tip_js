491688339ed80a764162ef6ebdfc7089e88bf4ba
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-03-31 17:13:55 -0300

    ajustes para exibição de valores

--- projeto/js/scripts.js
+++ projeto/js/scripts.js
@@ -1,8 +1,13 @@
 function update() {
     let bill = Number(document.getElementById("yourBill").value)
-    let tipPercent = document.getElementById("tipPercent").value
+    let tipPercent = document.getElementById("tipInput").value
     let split = document.getElementById("splitInput").value
     let tipValue = bill * (tipPercent/100)
     let billTotal = bill + tipValue
     let billEach = billTotal / split
+
+    document.getElementById("tipPercent").innerHTML = tipPercent + " %"
+    document.getElementById("tipValue").innerHTML = `$ ${tipValue}`
+
+    
 }
